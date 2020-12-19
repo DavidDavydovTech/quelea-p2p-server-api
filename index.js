c6936665634cb879506ad2d67472c1d67bb3bb31
David J. Davydov <davydovjd611@gmail.com>
2020-12-19 09:34:32 -0500

    Replace test route with graphql boilerplate code

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const compression = require('compression');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 app.use(cookieParser());
 app.use(cors());
 app.use(compression());
 
-app.route('/')
-  .get((req, res) => {
-    res.status(200).send('Hi from the API!')
-  });
+// GraphQL testing zone
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+app.use('/', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+// End of GraphQL testing zone
 
 module.exports = app;
